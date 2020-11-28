08f7413e3ea979f984779f82c462c3bcaed88a0f
HELEN HARMAN <hlharman@bellsouth.net>
2020-11-28 19:53:56 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,6 @@
 require 'pry'
 
+
 def my_find(collection)
 i = 0
   while i < collection.length
@@ -10,6 +11,6 @@ i = 0
   end
 end
 
-collection = (1..100).to_a
+
 my_find(collection) {|i| (i % 3 == 0) and  (i % 5 == 0)}
 end 
